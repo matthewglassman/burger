13d3b30e4d5ecaeda2a2f7f9e6f1da400029b1b8
matthewglassman <matthewglassmn78@gmail.com>
2017-03-13 18:28:19 -0400

    Added some basic code to server.js, connection.js and main.handlebars

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var express = require('express');
-var bodyparser = require('body-parser');
-var method = require('method-override');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
 
 var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+var port = 3000;
+app.list(port);
