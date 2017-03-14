c5153b3f05f578955a86efe5bf4286e1dde6013e
matthewglassman <matthewglassmn78@gmail.com>
2017-03-13 21:29:04 -0400

    Starting to code ORM.js

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
 
 var app = express();
+var port = 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-var port = 3000;
+
+var routes = require("./controllers/burgers_controller.js");
+app.use("/", routes);
+
 app.listen(port);
