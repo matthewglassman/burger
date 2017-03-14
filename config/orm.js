1bd0a9e56a8776b84ae8ef7b4718beca31b439c1
matthewglassman <matthewglassmn78@gmail.com>
2017-03-14 13:26:57 -0400

    Coded Orms files

--- config/orm.js
+++ config/orm.js
@@ -10,5 +10,65 @@ function printQuestionMarks(num) {
 	return arr.toString();
 }
 
+function objToSql(ob){
+	var arr = [];
+
+	for (var key in ob){
+		if (Object.hasOwnProperty.call(ob, key)) {
+			arr.push(key + "=" + ob[key]);
+		}
+	}
+
+	return arr.toString();
+}
+
+var orm = {
+	all: function(tableInput, cb){
+		var queryString = "SELECT * FROM " + tableInput + ";";
+		connection.query(queryString, function(err, result){
+			if (err) {
+				trhow err;
+			}
+			cb(result);
+		});
+	},
+
+	create: function(table, cols, vals, cb){
+		var queryString = "INSERT INTO " + table;
+
+		queryString += " (";
+		queryString += cols.toString();
+		queryString += ") ";
+		queryString += "VALUES (";
+		queryString += printQuestionMarks(vals.length);
+		queryString += ") ";
+
+		console.log(queryString);
+
+		connection.query(queryString, vals, function(err, result) {
+			if (err){
+				throw err;
+			}
+			cb(result);
+		});
+	},
+
+	update: function(table, objColVals, condition, cb){
+		var queryString = "UPDATE " + table;
+
+		queryString += " SET ";
+		queryString +=	objToSql(objColVals);
+		queryString += " WHERE ";
+		queryString += condition;
+
+		console.log(queryString);
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+			cb(result);
+		});
+	}
+};
 
 module.exports = orm;
